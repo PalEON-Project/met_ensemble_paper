e88b1773b0cc812d60f057f893addd725e7ffbb4
Christy Rollinson <crollinson@gmail.com>
2017-11-27 14:50:47 -0600

    Initial steps of met conversion

---
+++ ED_runs/0_setup/process_paleon_met_ensembles.R
@@ -0,0 +1,23 @@
+# Use a modified version of the pecan met2model.ED2.R to convert the met we want for this example
+
+# 1. Read in the csv with the ensembles members we want to convert
+ens.list <- read.csv(file = "Ensemble_Order.csv", na.strings="")
+ens.list
+# summary(ens.list)
+
+raw.base <- "~/met_ensemble/data/met_ensembles/HARVARD/1hr/ensembles"
+out.base <- "~/met_ensemble_paper/ED_runs/MetEnsemble_ED"
+
+if(!dir.exists(out.base)) dir.create(out.base, recursive = T, showWarnings = F)
+
+# Source the pecan function & loop through the ensembles we want
+source("met2model.ED2.R")
+for(i in 1:nrow(ens.list)){
+  met2model.ED2(in.path=file.path(raw.base, ens.list[i,"GCM"], ens.list[i,"EnsID"]), 
+                in.prefix=ens.list[i, "EnsID"], 
+                outfolder=file.path(out.base, ens.list[i, "EnsID"]), 
+                start_date="0850-01-01", end_date="2015-12-31", lst = 0, 
+                lat = NA, lon = NA, overwrite = FALSE, verbose = FALSE)
+  
+}
+  
